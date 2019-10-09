9f8b8e0d8a3cc9617ffa95d217959f70c3b1962b
Nicole2512 <53039255+Nicole2512@users.noreply.github.com>
2019-10-09 11:57:57 +0300

    Initial commit

---
+++ app/src/main/java/android/example/location_service/GetNearbyPlaces.java
@@ -0,0 +1,18 @@
+package android.example.location_service;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public class GetNearbyPlaces extends AsyncTask<Object,String,String> {
+    String googlePlacesData ;
+    GoogleMap googleMap;
+    String url ;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        return null;
+    }
+
+
+}
